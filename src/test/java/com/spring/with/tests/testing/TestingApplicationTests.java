7f92824005ccb3f932d1de2a4e8a8bfd6c68ad56
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-25 19:39:59 +0100

    Running with Spring

---
+++ src/test/java/com/spring/with/tests/testing/TestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.with.tests.testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
