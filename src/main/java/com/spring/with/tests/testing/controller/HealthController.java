1ea2f6995d639d20bb6b614da7e9e2cb9126fe68
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-09-25 19:45:10 +0100

    Move controller

--- src/main/java/com/spring/with/tests/testing/controller/HealthController.java
+++ src/main/java/com/spring/with/tests/testing/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.spring.with.tests.testing;
+package com.spring.with.tests.testing.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
