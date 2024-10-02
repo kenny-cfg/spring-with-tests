84c4dea6336fa156c6e84bfdd71aa9ff500599dd
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-10-02 19:31:30 +0100

    .env working

--- src/main/java/com/spring/with/tests/testing/controller/HealthController.java
+++ src/main/java/com/spring/with/tests/testing/controller/HealthController.java
@@ -1,5 +1,7 @@
 package com.spring.with.tests.testing.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("health")
 public class HealthController {
+    @Value("${bumPoo}")
+    private String testValue;
+
     @GetMapping
     public ResponseEntity<String> health() {
-        return ResponseEntity.ok("I'm healthy");
+        return ResponseEntity.ok("The test value is " + testValue);
     }
 }
