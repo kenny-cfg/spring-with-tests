77f4c8bf33fa239dad5ababcdcd91206e52a9a40
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-10-02 19:33:40 +0100

    .env with DB secrets

--- src/main/java/com/spring/with/tests/testing/controller/HealthController.java
+++ src/main/java/com/spring/with/tests/testing/controller/HealthController.java
@@ -1,7 +1,5 @@
 package com.spring.with.tests.testing.controller;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +8,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("health")
 public class HealthController {
-    @Value("${bumPoo}")
-    private String testValue;
 
     @GetMapping
     public ResponseEntity<String> health() {
-        return ResponseEntity.ok("The test value is " + testValue);
+        return ResponseEntity.ok("I'm here");
     }
 }
